6b2e147e82e4d7b214ca891bf36864008c6effb4
Thom Bruce <thom@thombruce.com>
2022-02-01 18:45:09 +0000

    Clean markup

--- packages/app/pages/donate/_address.vue
+++ packages/app/pages/donate/_address.vue
@@ -1,15 +1,19 @@
 <template lang='pug'>
 div
-  h1(v-if='displayName') {{ displayName }}
-  p(v-if='message') {{ message }}
-  input.px-4.py-3.rounded-full(
-    type='number'
-    min='0'
-    step='.0005'
-    v-model='eth'
-    class='bg-gray-300 dark:bg-gray-800'
-  )
-  CandyFlossDonateButton.rounded-full.py-2.px-3.bg-blue-400(:to='address' :value='eth')
+  form
+    div
+      h1(v-if='displayName') {{ displayName }}
+      p(v-if='message') {{ message }}
+    div
+      input(
+        type='number'
+        min='0'
+        step='.0005'
+        v-model='eth'
+        class='bg-gray-300 dark:bg-gray-800'
+      )
+    div
+      CandyFlossDonateButton.py-2.px-3.bg-blue-400(:to='address' :value='eth')
 </template>
 
 <script>
@@ -19,7 +23,7 @@ export default {
       displayName: this.$route.query.n,
       message: this.$route.query.m,
       address: this.$route.params.address,
-      eth: '.005'
+      eth: this.$route.query.e || '.005'
     }
   }
 }
