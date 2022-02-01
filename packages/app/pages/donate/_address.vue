8cef5ea2d0d28aa891ba351e02badaf59ae7f43f
Thom Bruce <thom@thombruce.com>
2022-02-01 23:29:38 +0000

    Better visuals

--- packages/app/pages/donate/_address.vue
+++ packages/app/pages/donate/_address.vue
@@ -1,22 +1,36 @@
 <template lang='pug'>
 div.text-center
   form(@submit.prevent='donate()')
-    div.py-10.text-4xl(v-if='showBalance')
+    div.pt-10.pb-3.text-2xl(v-if='showBalance')
       RockBandSimpleIcon(icon='ethereum')
       CandyFlossWalletBalance(:address='address')
+
     div
       h1(v-if='displayName') {{ displayName }}
+      p(class="mt-2 text-sm text-gray-500")
+        | {{ address }}
       p(v-if='message') {{ message }}
+
+
+    RockBandFormInput.my-3(
+      v-model='eth'
+      id='amount'
+      label='Amount'
+      placeholder='0.00'
+      type='number'
+      min='0'
+      step='any'
+    )
+      template(#prefix)
+        RockBandSimpleIcon(icon='ethereum')
+
     div
-      input(
-        type='number'
-        min='0'
-        step='any'
-        v-model='eth'
-        class='bg-gray-300 dark:bg-gray-800'
-      )
+      button.my-3(type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500")
+        | Donate {{ this.eth }} Eth
+
     div
-      button.py-2.px-3.bg-blue-400(type='submit') Donate {{ this.eth }} Eth
+      p(class="mt-2 text-sm text-gray-500")
+        | CandyFloss will connect with your wallet to finalise the transaction. Always make sure that you are sending the correct amount to the address you mean to. CandyFloss and its developers accept no liability for lost funds.
 </template>
 
 <script>
@@ -28,7 +42,7 @@ export default {
       message: this.$route.query.m,
       address: this.$route.params.address,
       showBalance: this.$route.query.b,
-      eth: this.$route.query.e || '.005'
+      eth: this.$route.query.e || ''
     }
   },
   computed: {
--- packages/app/pages/donate/new.vue
+++ packages/app/pages/donate/new.vue
@@ -1,29 +1,46 @@
 <template lang='pug'>
 div
-  form
-    div
-      label Name
-      br
-      input(type='text' v-model='displayName')
-    div
-      label Message
-      br
-      textarea(v-model='message')
-    div
-      label Address
-      br
-      input(type='text' v-model='address' required)
-    div
-      label Display Balance
-      br
-      input(type='checkbox' v-model='showBalance')
-    div
-      label Default Amount
-      br
-      input(type='number' min='0' step='.0005' v-model='defaultEth')
+  form(@submit.prevent='create()')
+    RockBandFormInput.my-3(
+      v-model='displayName'
+      id='displayName'
+      label='Name'
+      placeholder='John Doe'
+    )
+    RockBandFormTextarea.my-3(
+      v-model='message'
+      id='message'
+      label='Message'
+      placeholder='Hello, World!'
+      info='Add a short message to your donate page.'
+    )
+    RockBandFormInput.my-3(
+      v-model='address'
+      id='address'
+      label='Address'
+      placeholder='0x0000000000000000000000000000000000000000'
+    )
+    RockBandFormCheckbox.my-3(
+      v-model='showBalance'
+      id='showBalance'
+      label='Display Balance'
+      info='Show your wallet\'s current balance on the donate page.'
+    )
+    RockBandFormInput.my-3(
+      v-model='defaultEth'
+      id='defaultAmount'
+      label='Default Amount'
+      placeholder='0.00'
+      type='number'
+      min='0'
+      step='any'
+    )
+      template(#prefix)
+        RockBandSimpleIcon(icon='ethereum')
 
     div(v-if='previewLink')
-      NuxtLink(:to='previewLink') Create
+      button.my-3(type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500")
+        | Create
 
     div(v-if='asString')
       h3 Your Custom URL
@@ -34,11 +51,11 @@ div
 export default {
   data () {
     return {
-      displayName: null,
-      message: null,
-      address: null,
+      displayName: '',
+      message: '',
+      address: '',
       showBalance: false,
-      defaultEth: null
+      defaultEth: ''
     }
   },
   computed: {
@@ -53,6 +70,9 @@ export default {
     },
     asString () {
       if (this.previewLink) return process.env.baseUrl + this.$router.resolve(this.previewLink).href
+    },
+    create () {
+      this.$router.push(this.previewLink)
     }
   }
 }
