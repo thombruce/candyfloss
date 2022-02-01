e7e772b1e9df33fe841cdd66846c4059ddd43daa
Thom Bruce <thom@thombruce.com>
2022-02-01 18:17:03 +0000

    Add form-input classes to form elements

--- packages/app/pages/donate/new.vue
+++ packages/app/pages/donate/new.vue
@@ -4,19 +4,19 @@ div
     div
       label Name
       br
-      input(type='text' v-model='displayName')
+      input.form-input(type='text' v-model='displayName')
     div
       label Message
       br
-      textarea(v-model='message')
+      textarea.form-textarea(v-model='message')
     div
       label Address
       br
-      input(type='text' v-model='address')
+      input.form-input(type='text' v-model='address')
     div
       label Default Amount
       br
-      input(type='number' min='0' step='.0005' v-model='defaultEth')
+      input.form-input(type='number' min='0' step='.0005' v-model='defaultEth')
 
     div
       button(@click='create') Create
